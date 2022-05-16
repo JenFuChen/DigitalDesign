c3efc3be3a626c5daaf7ac55f97796ef4e239a00
Chen JenFu <dht98010@gmail.com>
2022-05-16 16:16:14 +0800

    0.0.2

--- 0516HW5/Average.v
+++ 0516HW5/Average.v
@@ -9,26 +9,28 @@ reg [7:0] form1[15:0][7:0]; //  存資料
 reg [7:0] form2[14:0][7:0]; //  輸出的
 integer col = 0;
 integer row = 0;
+integer cnt = 0;
 integer i,j;
 //開始存資料
 always @(clk)
 begin
     form1[row][col] = data;
     col = col + 1;
+	 cnt = cnt + 1;
     if( col % 8 == 0) 
     begin
         row = row + 1 ;
         col = 0;
     end
-end
-
-for(i = 0; i <= 14; i = i + 1)
-begin
-    for(j = 0; j <= 7 ; j = j + 1)begin
-        sum = (form1[i][j] + form1[i+1][j]);
-        form2[i][j] = sum / 2;
+	 if(cnt >= 127)
+	 begin
+		 for(i = 0; i <= 14; i = i + 1)begin
+			for(j = 0; j <= 7 ; j = j + 1)begin
+				  assign sum = (form1[i][j] + form1[i+1][j]) /2 ;
+				  form2[i][j] = sum ;
+			end
+		end
     end
 end
-
 //===================== Your Design =====================
 endmodule
