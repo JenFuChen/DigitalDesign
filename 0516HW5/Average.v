b39cb2f2ed5f99b771664232a9397c80dbc9e975
Chen Jenfu <dht98010@gmail.com>
2022-05-16 17:36:06 +0800

    jwnej

--- 0516HW5/Average.v
+++ 0516HW5/Average.v
@@ -1,37 +1,43 @@
 module Average(clk, reset, data, valid, out);
 input        clk, reset;
 input  [7:0] data;
-output reg[1:0] valid;
+output reg valid;
 output reg[7:0] out;
 //===================== Your Design =====================
 reg [8:0] sum;
-reg [7:0] form1[127:0]; //  å­˜è¼¸å…¥è³‡æ–™
-reg [7:0] a,b; //   åšåŠ æ¸›
+reg [7:0] form1[127:0]; //  ¦s¿é¤J¸ê®Æ
+reg [7:0] a,b; //   °µ¥[´î
 integer cnt = 0;
-integer i,j;
-//é–‹å§‹å­˜è³‡æ–™
+integer i;
+//¶}©l¦s¸ê®Æ
 always @(posedge clk)
 begin
     if(reset)
     begin
+        valid = 1'b0;
+        out = 8'b0000000;
         for(i = 0; i <127;i = i + 1)begin
-            form1[i] = 8'b00000000;
+            form1[i] <= 8'b00000000;
         end
     end
     else begin
+        // 128 µ§¸ê®Æ¦s¤J
         if(cnt <=127) begin
-            form1[cnt] = data;
-            cnt = cnt + 1;
-            valid = 1'b0;
+            form1[cnt] <= data;
+            cnt <= cnt + 1;
+            valid <= 1'b0;
         end
+        // ¦s§¹¤§«á§ï°µ³oÃä
         else begin
             for(i = 0; i <= 119; i = i + 1)begin
                 a = form1[i];
                 b = form1[i+8];
                 sum = a + b;
-                out = sum[8:1]+sum[0];
-                if(clk)
+                if(clk == 1)
+                begin   
                     valid = 1'b1;
+                    out = (sum >> 1) + sum[0];
+                end
                 else
                     valid = 1'b0;
             end
