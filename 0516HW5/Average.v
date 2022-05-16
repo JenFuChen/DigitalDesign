80b5fed65269293d9f580800ce931f885a09b979
Chen Jenfu <dht98010@gmail.com>
2022-05-16 15:26:05 +0800

    upload file

---
+++ 0516HW5/Average.v
@@ -0,0 +1,9 @@
+module Average(clk, reset, data, valid, out);
+input        clk, reset;
+input  [7:0] data;
+output       valid;
+output [7:0] out;
+//===================== Your Design =====================
+
+//===================== Your Design =====================
+endmodule
